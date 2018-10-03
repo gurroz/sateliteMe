0bc07f9fedbd62cbe501a047d9fe71169ce5adaf
Gonzalo Urroz <gonzalo.urroz@cursor.cl>
2018-10-03 21:00:40 +1000

    Calls aPI to get destination

--- app/src/main/java/rmit/mad/project/view/TrackableDetailActivity.java
+++ app/src/main/java/rmit/mad/project/view/TrackableDetailActivity.java
@@ -6,7 +6,8 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,6 +25,7 @@ import java.util.List;
 import rmit.mad.project.R;
 import rmit.mad.project.model.RouteInfo;
 import rmit.mad.project.model.Trackable;
+import rmit.mad.project.service.DistanceCalculationTask;
 import rmit.mad.project.service.RouteInfoService;
 import rmit.mad.project.service.TrackableService;
 
@@ -36,11 +38,13 @@ public class TrackableDetailActivity extends AppCompatActivity {
     private TextView categoryView;
     private TextView descriptionView;
     private ImageView imageView;
+    private Button getDistanceBtn;
 
     private Trackable trackable;
 
     MapView mMapView;
     private GoogleMap googleMap;
+    List<RouteInfo> routesInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +59,17 @@ public class TrackableDetailActivity extends AppCompatActivity {
         categoryView = findViewById(R.id.category);
         descriptionView = findViewById(R.id.description);
         imageView = findViewById(R.id.trackableImage);
+        getDistanceBtn = findViewById(R.id.getDistance);
+
+        // TODO: Change this to the RouteInfoService.getInstance().getTrackableRouteInfoFromNow for non testing running.
+        routesInfo = RouteInfoService.getInstance().getTrackableRouteInfoTest(this, trackable.getId(), 100);
+
+        getDistanceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DistanceCalculationTask().execute("-37.807425,144.963814", routesInfo.get(0).getLocation());
+            }
+        });
 
 
         mMapView = findViewById(R.id.mapView);
@@ -81,8 +96,6 @@ public class TrackableDetailActivity extends AppCompatActivity {
     }
 
     private void showMap() {
-        // TODO: Change this to the RouteInfoService.getInstance().getTrackableRouteInfoFromNow for non testing running.
-        final List<RouteInfo> routesInfo = RouteInfoService.getInstance().getTrackableRouteInfoTest(this, trackable.getId(), 100);
 
         mMapView.getMapAsync(new OnMapReadyCallback() {
             @Override
@@ -120,16 +133,7 @@ public class TrackableDetailActivity extends AppCompatActivity {
     private void checkMapPermission() {
         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-            }
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
         } else {
             showMap();
         }
