87050d20d71defc0e3bb3b70a3558e711cf6f6ca
Gonzalo Urroz <gonzalo.urroz@cursor.cl>
2018-10-03 19:07:18 +1000

    Adds settings to save shared preferences

--- app/src/main/java/rmit/mad/project/view/HomeActivity.java
+++ app/src/main/java/rmit/mad/project/view/HomeActivity.java
@@ -1,10 +1,14 @@
 package rmit.mad.project.view;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TabHost;
 
 import java.io.IOException;
@@ -12,6 +16,9 @@ import java.io.InputStream;
 
 import rmit.mad.project.R;
 import rmit.mad.project.service.TrackableService;
+
+import static rmit.mad.project.enums.IntentModelEnum.ROUTE_INFO;
+import static rmit.mad.project.enums.IntentModelEnum.TRACKABLE_ID;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -22,6 +29,9 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
         tabHost = findViewById(android.R.id.tabhost);
         tabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
@@ -55,5 +65,30 @@ public class HomeActivity extends AppCompatActivity {
         } catch (IOException e) {
             Log.e(TAG, "Error reading food truck files: {}", e);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_trackables, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            this.startActivity(intent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
